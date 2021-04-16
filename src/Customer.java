c805a98a3253d534c1f3c567e90f90ca7a43b642
Fatemeh Ghoreyshi <gho21920@spengergasse.at>
2021-04-16 13:31:22 +0100

    Customer

---
+++ src/Customer.java
@@ -0,0 +1,32 @@
+
+public class Custome {
+	private int customerID;
+	private String firstName;
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Custome(int customerID, String firstName) {
+		super();
+		this.customerID = customerID;
+		this.firstName = firstName;
+	}
+	
+	
+	
+	
+
+}
